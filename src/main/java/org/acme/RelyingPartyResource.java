4fb4e1e4a144f8aadefbde7f1a3ba90fe7017d97
Hinse ter Schuur <582538+coduinix@users.noreply.github.com>
2022-12-15 14:46:39 +0100

    Add endpoint to fetch metadata for a specific OIDC clientId or SAML entityId

--- src/main/java/org/acme/RelyingPartyResource.java
+++ src/main/java/org/acme/RelyingPartyResource.java
@@ -1,11 +1,15 @@
 package org.acme;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.acme.model.OidcRelyingParty;
 import org.acme.model.RelyingParty;
+import org.acme.model.SamlRelyingParty;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
 import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Set;
@@ -22,6 +26,28 @@ public class RelyingPartyResource {
     @GET
     public RelyingPartyResult list() {
         return RelyingPartyResult.of(relyingParties);
+    }
+
+    @GET
+    @Path("/oidc/{clientId}")
+    public Response findOidcRp(@PathParam("clientId") String clientId) {
+        return relyingParties.stream()
+                .filter(rp -> rp instanceof OidcRelyingParty orp && orp.clientId().equals(clientId))
+                .findFirst()
+                .map(Response::ok)
+                .orElseGet(() -> Response.status(Response.Status.NOT_FOUND))
+                .build();
+    }
+
+    @GET
+    @Path("/saml/{entityId}")
+    public Response findSamlRp(@PathParam("entityId") String entityId) {
+        return relyingParties.stream()
+                .filter(rp -> rp instanceof SamlRelyingParty orp && orp.entityId().equals(entityId))
+                .findFirst()
+                .map(Response::ok)
+                .orElseGet(() -> Response.status(Response.Status.NOT_FOUND))
+                .build();
     }
 }
 
