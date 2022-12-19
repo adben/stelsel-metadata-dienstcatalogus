c5e7a461089a579f2cdef83515962f051e5230b3
Hinse ter Schuur <582538+coduinix@users.noreply.github.com>
2022-12-19 09:42:11 +0100

    Make redirectUrls List instead of array type

--- src/main/java/org/acme/model/OidcRelyingParty.java
+++ src/main/java/org/acme/model/OidcRelyingParty.java
@@ -2,6 +2,8 @@ package org.acme.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.List;
+
 public record OidcRelyingParty(
         @JsonProperty(required = true)
         String name,
@@ -15,8 +17,8 @@ public record OidcRelyingParty(
         @JsonProperty(value = "jwks_uri", required = true)
         String jwksUri,
         @JsonProperty(value = "redirect_uris", required = true)
-        String[] redirectUris,
+        List<String> redirectUris,
         @JsonProperty(value = "post_logout_redirect_uri", required = true)
-        String[] postLogoutRedirectUris
+        List<String> postLogoutRedirectUris
 ) implements RelyingParty {
 }
